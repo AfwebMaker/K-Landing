f51b8790f17af49479e55f79be794632d0d58aa5
whh32 <am.lashkari2017@gmail.com>
2023-06-09 16:23:52 +0330

    complete nav level1

--- src/layout/Header/Header.js
+++ src/layout/Header/Header.js
@@ -1,26 +1,46 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 //assets
 import KargoshaLogo from '../../assets/images/header/Kargosha-Logo.svg'
 import person from '../../assets/images/header/person.svg'
+import search from '../../assets/images/header/Search.svg'
+import Navigation from './Navigation'
 
 function Header() {
-  return (
-    <header className='w-full fcc' >
-      <nav className='maxLayout flex justify-between items-center w-full h-[70px] py-5 mx-5'>
-        <div className='h-full'>
-          <img className='h-full' src={KargoshaLogo} alt="لوگو کارگشا" />
-        </div>
+  const [menuIsActive, setMenuIsActive] = useState(false);
 
-        <div className='fcc text-sm'>
-          <div className='ml-4 flex'>
-            <span>متخصص هستم</span>
-            <img src={person} alt="ایکون شخص" />
+  //detect scrollTop largest 100 or not
+  useEffect(() => {
+    const onScroll = window.addEventListener('scroll', (e) => {
+      setMenuIsActive(e.target.documentElement.scrollTop > 100);
+    });
+
+    return () => window.removeEventListener('scroll', onScroll);
+  }, [menuIsActive]);
+
+  return (
+    <header className={`w-full flex justify-start items-center fixed z-20 bg-white flex-col h-[100px] ${menuIsActive ? 'shadow-xl' : ''}`} >
+      <div className={`maxLayout flex justify-between items-center w-full px-5 z-10 transition-all bg-white ${menuIsActive ? 'h-[60px]' : 'h-full'}`}>
+        <div className='h-full fcc'>
+          <img className='h-[36px] ml-6' src={KargoshaLogo} alt="لوگو کارگشا" />
+          <div className={`relative transition-all overflow-hidden fcc h-[37px] ${menuIsActive ? 'w-[550px]' : 'w-0'}`}>
+            <img className='h-[50%] m-4 right-0 absolute' src={search} alt="ایکون سرچ" />
+            <input className={`bg-color-1 h-full w-full font-light text-sm rounded-lg pr-12 focus:outline-0 `} type="text" placeholder='خدمت ساختمانی که به دنبال آن هستید را وارد کنید.' />
           </div>
-          <button className='bg-color-font-3 rounded-md text-white p-2'>
-            ورود یا عضویت
+        </div>
+        <div className='fcc text-sm'>
+          <div className='ml-4 fcc'>
+            <img className='h-4' src={person} alt="ایکون متخصص هستم" />
+            <span className='mr-2 text-color-font-2'>متخصص هستم</span>
+          </div>
+          <button className='bg-color-font-3 rounded-md text-white p-2 font-thin'>
+            <span className='font-medium m-1'>ورود</span>
+            یا
+            <span className='font-medium m-1'>عضویت</span>
           </button>
         </div>
-      </nav>
+      </div>
+
+      <Navigation />
     </header>
   )
 }
---
+++ src/layout/Header/Navigation.js
@@ -0,0 +1,40 @@
+import React from 'react'
+//assets
+import screwdriver from '../../assets/images/header/Screwdriver.svg'
+import file from '../../assets/images/header/File.svg'
+import friends from '../../assets/images/header/Friends.svg'
+import wallet from '../../assets/images/header/Wallet.svg'
+
+function Navigation() {
+    return (
+        <nav className='absolute maxLayout w-full flex bg-white h-[40px] top-[60px] px-5'>
+            <ul className='fcc h-full text-color-font-2 font-regular'>
+                <li className='fcc pl-6 cursor-pointer'>
+                    <img className='ml-2' src={screwdriver} alt="ایکون خدمات ساختمانی" />
+                    <span>خدمات ساختمانی</span>
+                </li>
+                <li className='fcc px-6 cursor-pointer'>
+                    <span>خدمات فوری</span>
+                </li>
+                <li className='fcc px-6 cursor-pointer'>
+                    <span>بازسازی</span>
+                </li>
+                <div className='w-[2px] h-3 bg-color-2'></div>
+                <li className='fcc px-6 cursor-pointer'>
+                    <img className='ml-2' src={file} alt="ایکون فروشگاه فایل" />
+                    <span>فروشگاه فایل</span>
+                </li>
+                <li className='fcc px-6 cursor-pointer'>
+                    <img className='ml-2' src={friends} alt="ایکون دیده شو" />
+                    <span>دیده شو</span>
+                </li>
+                <li className='fcc px-6 cursor-pointer'>
+                    <img className='ml-2' src={wallet} alt="ایکون قیمت های روز" />
+                    <span>قیمت های روز</span>
+                </li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Navigation
