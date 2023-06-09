b69c51f29bfa2013553d1aa7cbcceb6d26b4ae4e
Amir Hossein Saharkhiz <sahar.khiz.1379@gmail.com>
2023-06-09 12:53:28 +0330

    end weblog

--- src/components/Home/weblog/Weblog.js
+++ src/components/Home/weblog/Weblog.js
@@ -4,13 +4,13 @@ import BlogCards from './BlogCards';
 
 function Weblog() {
   return (
-    <div className="text-center flex items-center flex-col py-10">
+    <div className="text-center flex items-center flex-col pt-10 pb-16">
       <div className="max-w-[1300px] px-[20px] md:px-[32px] w-full flex flex-col items-center justify-center">
         <div className='w-full flex items-center justify-between mb-10'>
-          <h2 className='text-color-font-3 font-bold text-lg fcc'>
+          <h2 className='text-color-font-3 font-bold text-xl fcc'>
             وبلاگ
           </h2>
-          <span className='text-color-font-4 font-bold text-sm fcc'>
+          <span className='text-color-font-4 font-bold text-sm fcc cursor-pointer'>
             بیشتر...
           </span>
         </div>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,7 +4,7 @@ import Weblog from '../components/Home/weblog/Weblog.js'
 
 export const Home = () => {
   return (
-    <div className='bg-orange-400'>
+    <div>
         <Weblog/>
     </div>
   )
