991cc477db96d72a4ac2832f4cc47d4f6abf71e7
Amir Hossein Saharkhiz <sahar.khiz.1379@gmail.com>
2023-06-17 21:26:55 +0330

    .

--- src/components/Home/weblog/BlogCards.js
+++ src/components/Home/weblog/BlogCards.js
@@ -1,6 +1,11 @@
 import React from 'react'
 // components
 import BlogCard from "./BlogCard"
+// ASSETS
+import blog1 from "../../../assets/images/home/weblog/1.png"
+import blog2 from "../../../assets/images/home/weblog/2.png"
+import blog3 from "../../../assets/images/home/weblog/3.png"
+import blog4 from "../../../assets/images/home/weblog/4.png"
 
 const products = [
   {
@@ -8,7 +13,7 @@ const products = [
     title: "هزینه نگهداری استخر خانگی چقدر است و چگونگی کاهش هزینه",
     description: "قبل از آنکه در محوطه خانه خود یک استخر زیبا احداث کنید، مهم است...",
     href: 'https://wiki.kargosha.com/%d9%87%d8%b2%db%8c%d9%86%d9%87-%d9%86%da%af%d9%87%d8%af%d8%a7%d8%b1%db%8c-%d8%a7%d8%b3%d8%aa%d8%ae%d8%b1-%d8%ae%d8%a7%d9%86%da%af%db%8c/',
-    imageSrc: "https://cdn.kargosha.com/user/2022-11/1-18-780x470-1.jpg",
+    imageSrc: blog1,
     imageAlt:
       'Payment application dashboard screenshot with transaction table, financial highlights, and main clients on colorful purple background.',
   },
@@ -17,7 +22,7 @@ const products = [
     title: "شارژ گاز کولر گازی چیست و چه زمانی انجام می‌شود؟",
     description: "در این مقاله از ویکی ساختمون می‌خواهیم به شارژ گاز کولر گازی بپردازیم...",
     href: 'https://wiki.kargosha.com/%d8%b4%d8%a7%d8%b1%da%98-%da%af%d8%a7%d8%b2-%da%a9%d9%88%d9%84%d8%b1-%da%af%d8%a7%d8%b2%db%8c/',
-    imageSrc: "https://cdn.kargosha.com/user/2022-11/7-780x470.jpeg",
+    imageSrc: blog2,
     imageAlt:
       'Payment application dashboard screenshot with transaction table, financial highlights, and main clients on colorful purple background.',
   },
@@ -26,7 +31,7 @@ const products = [
     title: "برای داشتن بهترین آشپزخانه چی رو کجا بزاریم؟!",
     description: "چیدمان کابینت نه تنها کار دشواری نیست، بلکه برای بسیاری از بانوان و دختران...",
     href: 'https://wiki.kargosha.com/%da%86%db%8c%d8%af%d9%85%d8%a7%d9%86-%da%a9%d8%a7%d8%a8%db%8c%d9%86%d8%aa/',
-    imageSrc: "https://wiki.kargosha.com/wp-content/uploads/2020/07/2-11-780x470.jpg",
+    imageSrc: blog3,
     imageAlt:
       'Payment application dashboard screenshot with transaction table, financial highlights, and main clients on colorful purple background.',
   },
@@ -35,7 +40,7 @@ const products = [
     title: "راهنمای خرید چمن مصنوعی",
     description: "قبل از خرید چمن مصنوعی می بایست به راهنمای آن و کیفیت محصول توجه کرد...",
     href: 'https://wiki.kargosha.com/%d8%b1%d8%a7%d9%87%d9%86%d9%85%d8%a7%db%8c-%d8%ae%d8%b1%db%8c%d8%af-%da%86%d9%85%d9%86-%d9%85%d8%b5%d9%86%d9%88%d8%b9%db%8c/',
-    imageSrc: "https://wiki.kargosha.com/wp-content/uploads/2022/08/2-390x220.jpg",
+    imageSrc: blog4,
     imageAlt:
       'Payment application dashboard screenshot with transaction table, financial highlights, and main clients on colorful purple background.',
   },
