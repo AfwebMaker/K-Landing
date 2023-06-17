3a1df3d57cfc85e1c4f6a9bc8ac934944ad36299
Amir Hossein Saharkhiz <sahar.khiz.1379@gmail.com>
2023-06-17 08:52:41 +0330

    .

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -11,6 +11,7 @@ const Routers = () => {
 
     return (
         <Routes>
+            
             <Route path='/' element={<Home />} />
             <Route path='/categories' element={<Categories />} />
             <Route path='/categories/:slug' element={<Category />} />
