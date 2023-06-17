a5f2ea21b08cfb13736ab12b2943d91816fdc5b8
whh32 <am.lashkari2017@gmail.com>
2023-06-17 19:46:24 +0330

    update calculator

--- src/components/Home/calculator/Calculator.js
+++ src/components/Home/calculator/Calculator.js
@@ -1,11 +1,13 @@
-import React, { useReducer } from 'react'
+import React, { useReducer, useRef } from 'react'
 //assets
 import calculator from '../../../assets/images/home/calculator/calculator.png'
+import rangeCircle from '../../../assets/images/home/calculator/range.svg'
 
 const initialState = {
     paymentPeriod: 'sixMonths',
     paymentType: 'threeMonths',
-    numberPayments: '3'
+    numberPayments: '3',
+    rangeValue: 0
 };
 
 const reducer = (state, action) => {
@@ -16,6 +18,8 @@ const reducer = (state, action) => {
             return { ...state, paymentType: action.value };
         case 'numberPaymentsHandler':
             return { ...state, numberPayments: action.value };
+        case 'rangeValueHandler':
+            return { ...state, rangeValue: action.value };
         default:
             return state
     }
@@ -23,6 +27,13 @@ const reducer = (state, action) => {
 
 function Calculator() {
     const [state, dispatch] = useReducer(reducer, initialState);
+    const range = useRef('')
+
+    //back ground of range
+    function progressScript() {
+        const sliderValue = range.current.value;
+        range.current.style.background = `linear-gradient(to right, #FF513D ${sliderValue}%, #ccc ${sliderValue}%)`;
+    }
 
     return (
 
@@ -44,46 +55,50 @@ function Calculator() {
                     </div>
                     <div className='w-full md:w-1/2 md:h-full flex items-center justify-center md:justify-end mt-10 md:mt-0 md:p-[1%]'>
                         <div className='w-full h-[350px] md:h-full bg-[#EBEFED] shadow-lg md:bg-white rounded-lg flex items-center justify-center max-w-lg flex-col'>
-                            <div className='w-full h-[80%] px-[4%] relative flex flex-col justify-around text-xs lg:text-sm'>
+                            <div className='w-full h-[80%] px-[4%] relative flex flex-col justify-around text-[10px] lg:text-sm'>
                                 <div className='w-full flex items-center justify-between h-[15%]'>
                                     <span className='text-color-font-3'>دوره پرداخت</span>
-                                    <div className='relative w-[35%] text-color-font-2 h-[70%] rounded-lg bg-white md:bg-color-1 flex items-center justify-center md:text-sm text-xs'>
-                                        <span onClick={() => dispatch({ type: 'paymentPeriodHandler', value: 'sixMonths' })} className={`text-xs lg:text-sm w-1/2 h-full z-10 fcc cursor-pointer ${state.paymentPeriod === 'sixMonths' ? 'text-white' : 'text-color-font-2'}`}>شش ماه</span>
-                                        <span onClick={() => dispatch({ type: 'paymentPeriodHandler', value: 'oneYear' })} className={`text-xs lg:text-sm w-1/2 h-full z-10 fcc cursor-pointer ${state.paymentPeriod === 'oneYear' ? 'text-white' : 'text-color-font-2'}`}>یکساله</span>
+                                    <div className='relative w-[35%] text-color-font-2 h-[60%] rounded-lg bg-white md:bg-color-1 flex items-center justify-center md:text-sm text-xs'>
+                                        <span onClick={() => dispatch({ type: 'paymentPeriodHandler', value: 'sixMonths' })} className={`text-[10px] lg:text-sm w-1/2 h-full z-10 fcc cursor-pointer ${state.paymentPeriod === 'sixMonths' ? 'text-white' : 'text-color-font-2'}`}>شش ماه</span>
+                                        <span onClick={() => dispatch({ type: 'paymentPeriodHandler', value: 'oneYear' })} className={`text-[10px] lg:text-sm w-1/2 h-full z-10 fcc cursor-pointer ${state.paymentPeriod === 'oneYear' ? 'text-white' : 'text-color-font-2'}`}>یکساله</span>
                                         <div className={`transition-all h-full w-1/2 bg-color-font-5 absolute rounded-lg ${state.paymentPeriod === 'oneYear' ? 'left-0' : 'left-[50%]'} `}></div>
                                     </div>
                                 </div>
                                 <div className='w-full flex items-center justify-between h-[15%]'>
                                     <span className='text-color-font-3'>نوع پرداخت</span>
-                                    <div className='relative w-[35%] text-color-font-2 h-[70%] rounded-lg bg-white md:bg-color-1  flex items-center justify-center lg:text-sm text-xs'>
-                                        <span onClick={() => dispatch({ type: 'paymentTypeHandler', value: 'Instant' })} className={`text-xs lg:text-sm w-1/2 h-full z-10 fcc cursor-pointer ${state.paymentType === 'Instant' ? 'text-white' : 'text-color-font-2'}`}>آنی</span>
-                                        <span onClick={() => dispatch({ type: 'paymentTypeHandler', value: 'threeMonths' })} className={`text-xs lg:text-sm w-1/2 h-full z-10 fcc cursor-pointer ${state.paymentType === 'threeMonths' ? 'text-white' : 'text-color-font-2'}`}>سه ماه</span>
+                                    <div className='relative w-[35%] text-color-font-2 h-[60%] rounded-lg bg-white md:bg-color-1  flex items-center justify-center lg:text-sm text-xs'>
+                                        <span onClick={() => dispatch({ type: 'paymentTypeHandler', value: 'Instant' })} className={`text-[10px] lg:text-sm w-1/2 h-full z-10 fcc cursor-pointer ${state.paymentType === 'Instant' ? 'text-white' : 'text-color-font-2'}`}>آنی</span>
+                                        <span onClick={() => dispatch({ type: 'paymentTypeHandler', value: 'threeMonths' })} className={`text-[10px] lg:text-sm w-1/2 h-full z-10 fcc cursor-pointer ${state.paymentType === 'threeMonths' ? 'text-white' : 'text-color-font-2'}`}>سه ماه</span>
                                         <div className={`transition-all h-full w-1/2 bg-color-font-5 absolute rounded-lg ${state.paymentType === 'threeMonths' ? 'left-0' : 'left-[50%]'} `}></div>
                                     </div>
                                 </div>
                                 <div className='w-full flex items-center justify-between h-[15%]'>
-                                    <span className='text-color-font-3 text-xs lg:text-sm'>تعداد پرداخت ها (فقره)</span>
-                                    <div className='relative w-[52.5%] text-color-font-2 h-[70%] rounded-lg bg-white md:bg-color-1  flex items-center justify-center'>
-                                        <span onClick={() => dispatch({ type: 'numberPaymentsHandler', value: '2' })} className={`text-xs lg:text-sm w-1/3 h-full z-10 fcc cursor-pointer ${state.numberPayments === '2' ? 'text-white' : 'text-color-font-2'}`}>۲ فقره</span>
-                                        <span onClick={() => dispatch({ type: 'numberPaymentsHandler', value: '3' })} className={`text-xs lg:text-sm w-1/3 h-full z-10 fcc cursor-pointer ${state.numberPayments === '3' ? 'text-white' : 'text-color-font-2'}`}>۳ فقره</span>
-                                        <span onClick={() => dispatch({ type: 'numberPaymentsHandler', value: '6' })} className={`text-xs lg:text-sm w-1/3 h-full z-10 fcc cursor-pointer ${state.numberPayments === '6' ? 'text-white' : 'text-color-font-2'}`}>٦ فقره</span>
+                                    <span className='text-color-font-3'>تعداد پرداخت ها (فقره)</span>
+                                    <div className='relative w-[52.5%] text-color-font-2 h-[60%] rounded-lg bg-white md:bg-color-1  flex items-center justify-center'>
+                                        <span onClick={() => dispatch({ type: 'numberPaymentsHandler', value: '2' })} className={`text-[10px] lg:text-sm w-1/3 h-full z-10 fcc cursor-pointer ${state.numberPayments === '2' ? 'text-white' : 'text-color-font-2'}`}>۲ فقره</span>
+                                        <span onClick={() => dispatch({ type: 'numberPaymentsHandler', value: '3' })} className={`text-[10px] lg:text-sm w-1/3 h-full z-10 fcc cursor-pointer ${state.numberPayments === '3' ? 'text-white' : 'text-color-font-2'}`}>۳ فقره</span>
+                                        <span onClick={() => dispatch({ type: 'numberPaymentsHandler', value: '6' })} className={`text-[10px] lg:text-sm w-1/3 h-full z-10 fcc cursor-pointer ${state.numberPayments === '6' ? 'text-white' : 'text-color-font-2'}`}>٦ فقره</span>
                                         <div className={`transition-all h-full w-1/3 bg-color-font-5 absolute rounded-lg ${state.numberPayments === '6' ? 'left-0' : state.numberPayments === '3' ? 'left-1/3' : 'left-2/3'}`}></div>
                                     </div>
                                 </div>
-                                <div className='w-full flex flex-col items-center justify-around h-[35%]'>
+                                <div className='w-full flex flex-col items-center justify-around h-[40%]'>
                                     <span className='text-color-font-3 flex w-full h-[20%]'>مبلغ در خواستی</span>
-                                    <div className='relative w-full h-[30%] fcc'>
-                                        <input className='w-full absolute' type="range" />
+                                    <div style={{ direction: 'ltr' }} className='relative w-full h-[30%] fcc'>
+                                        <input value={state.rangeValue} ref={range} onChange={(e) => { progressScript(); dispatch({ type: 'rangeValueHandler', value: e.target.value }) }} className='cursor-pointer z-10 slider w-full absolute' type="range" />
+
+                                        <div style={{ left: `${state.rangeValue-8}%` }} className='absolute left-0 top-[-10px] text-[10px] font-light text-color-2 w-auto whitespace-nowrap	'>
+                                            {28320600} تومان
+                                        </div>
                                     </div>
                                     <div className='relative w-full h-[30%]'>
-                                        <input className='focus:outline-none w-full h-full rounded-lg bg-[#DCDDDE]  pr-[4%]' placeholder='به طور مثال ۲۳,۹۸۰,۰۰۰' type="text" />
-                                        <div className='text-white w-[20%] h-full absolute left-0 bg-color-font-5 fcc top-0 rounded-lg cursor-pointer'>
+                                        <input className='focus:outline-none w-full h-full rounded-lg bg-[#DCDDDE] pr-[4%]' placeholder='به طور مثال ۲۳,۹۸۰,۰۰۰' type="text" />
+                                        <div className='text-white w-[20%] h-full absolute left-0 bg-color-font-5 fcc top-0 rounded-lg cursor-default'>
                                             تومان
                                         </div>
                                     </div>
                                 </div>
                             </div>
-                            <div className='relative w-full h-[20%] text-color-font-3 flex flex-col justify-around py-[2%] px-[4%] border-t-2 border-dashed border-t-color-2 text-xs lg:text-sm'>
+                            <div className='relative w-full h-[20%] text-color-font-3 flex flex-col justify-around py-[2%] px-[4%] border-t-2 border-dashed border-t-color-2 text-[10px] md:text-[1.2vw] lg:text-sm'>
                                 <div className='flex justify-between items-center'>
                                     <span>کمیسیون لیزینگ :</span>
                                     <span>۷,۰۰۰,۰۰۰ تومان</span>
